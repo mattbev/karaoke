2bca903f18947a4fdaa9c63043713bf6ba727eca
Sophia <sophias@mit.edu>
2018-05-03 21:56:32 -0400

    implemented testPiece2

--- test/karaoke/sound/SequencePlayerTest.java
+++ test/karaoke/sound/SequencePlayerTest.java
@@ -139,65 +139,149 @@ public class SequencePlayerTest {
        
     }
     
-//    //Covers:
-//    //      piece2.abc
-//    @Test
-//    
-//    public void testPiece2() throws MidiUnavailableException, InvalidMidiDataException {
-//        Instrument piano = Instrument.PIANO;
-//
-//        // create a new player
-//        final int beatsPerMinute = 120; // a beat is a quarter note, so this is 120 quarter notes per minute
-//        final int ticksPerBeat = 64; // allows up to 1/64-beat notes to be played with fidelity
-//        SequencePlayer player = new MidiSequencePlayer(beatsPerMinute, ticksPerBeat);
-//        
-//        // addNote(instr, pitch, startBeat, numBeats) schedules a note with pitch value 'pitch'
-//        // played by 'instr' starting at 'startBeat' to be played for 'numBeats' beats.
-//        
-//        int startBeat = 0;
-//        int numBeats = 1;
-//        player.addNote(piano, new Pitch('C'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('D'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('E'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('F'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('G'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('A'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('B'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('C').transpose(Pitch.OCTAVE), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('B'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('A'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('G'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('F'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('E'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('D'), startBeat++, numBeats);
-//        player.addNote(piano, new Pitch('C'), startBeat++, numBeats);
-//        
-//        // add a listener at the end of the piece to tell main thread when it's done
-//        Object lock = new Object();
-//        player.addEvent(startBeat, (Double beat) -> {
-//            synchronized (lock) {
-//                lock.notify();
-//            }
-//        });
-//        
-//        // print the configured player
-//        System.out.println(player);
-//
-//        // play!
-//        player.play();
-//        
-//        // wait until player is done
-//        // (not strictly needed here, but useful for JUnit tests)
-//        synchronized (lock) {
-//            try {
-//                lock.wait();
-//            } catch (InterruptedException e) {
-//                return;
-//            }
-//        }
-//        System.out.println("done playing");
-//        
-//    }
+    //Covers:
+    //      piece2.abc
+    @Test
+    
+    public void testPiece2() throws MidiUnavailableException, InvalidMidiDataException {
+        Instrument piano = Instrument.PIANO;
+
+        // create a new player
+        final int beatsPerMinute = 200; // a beat is a quarter note, so this is 120 quarter notes per minute
+        final int ticksPerBeat = 64; // allows up to 1/64-beat notes to be played with fidelity
+        SequencePlayer player = new MidiSequencePlayer(beatsPerMinute, ticksPerBeat);
+        
+        // addNote(instr, pitch, startBeat, numBeats) schedules a note with pitch value 'pitch'
+        // played by 'instr' starting at 'startBeat' to be played for 'numBeats' beats.
+        
+        double startBeat = 0;
+        int numBeats = 1;
+        player.addNote(piano, new Pitch('E').transpose(Pitch.OCTAVE), startBeat, .5);
+        player.addNote(piano, new Pitch('F').transpose(1), startBeat, .5);
+        startBeat += .5;
+        
+        
+        
+        player.addNote(piano, new Pitch('E').transpose(Pitch.OCTAVE), startBeat, .5);
+        player.addNote(piano, new Pitch('F').transpose(1), startBeat, .5);
+        
+        startBeat += .5;
+        
+        startBeat += .5;   // .5 beat rest
+        
+        player.addNote(piano, new Pitch('E').transpose(Pitch.OCTAVE), startBeat, .5);
+        player.addNote(piano, new Pitch('F').transpose(1), startBeat, .5);
+        startBeat += .5;
+        
+        startBeat += .5;   // .5 beat rest
+        
+        player.addNote(piano, new Pitch('C').transpose(Pitch.OCTAVE), startBeat, .5);
+        player.addNote(piano, new Pitch('F').transpose(1), startBeat, .5);
+        startBeat += .5;
+        
+        player.addNote(piano, new Pitch('E').transpose(Pitch.OCTAVE), startBeat, 1);
+        player.addNote(piano, new Pitch('F').transpose(1), startBeat, 1);
+        startBeat += 1;
+        
+        //end measure
+        
+        player.addNote(piano, new Pitch('G').transpose(Pitch.OCTAVE), startBeat, 1);
+        player.addNote(piano, new Pitch('B'), startBeat, 1);
+        player.addNote(piano, new Pitch('G'), startBeat, 1);
+        startBeat += 1;
+        
+        startBeat += 1;   // 1 beat rest
+        
+        player.addNote(piano, new Pitch('G'), startBeat, 1);
+        startBeat += 1;
+        
+        startBeat += 1;   // 1 beat rest
+        
+        //end measure
+        
+        player.addNote(piano, new Pitch('C').transpose(Pitch.OCTAVE), startBeat, 3.0/2);
+        startBeat += 3.0/2;
+        player.addNote(piano, new Pitch('G'), startBeat, .5);
+        startBeat += .5;
+        
+        startBeat += 1;   // 1 beat rest
+        
+        player.addNote(piano, new Pitch('E'), startBeat, numBeats);
+        startBeat += 1;
+        
+        //end measure
+        
+        player.addNote(piano, new Pitch('E'), startBeat, .5);
+        startBeat += .5;
+        player.addNote(piano, new Pitch('A'), startBeat, numBeats);
+        startBeat += 1;
+        player.addNote(piano, new Pitch('B'), startBeat, numBeats);
+        startBeat += 1;
+        player.addNote(piano, new Pitch('B').transpose(-1), startBeat, .5);
+        startBeat += .5;
+        player.addNote(piano, new Pitch('A'), startBeat, numBeats);
+        startBeat += 1;
+        
+        //end measure
+        
+        player.addNote(piano, new Pitch('G'), startBeat, 1.0/3);       
+        startBeat += 1.0/3;
+        player.addNote(piano, new Pitch('E').transpose(Pitch.OCTAVE), startBeat, 1.0/3);        
+        startBeat += 1.0/3;
+        player.addNote(piano, new Pitch('G').transpose(Pitch.OCTAVE), startBeat, 1.0/3);        
+        startBeat += 1.0/3;
+        //end triple
+        
+        player.addNote(piano, new Pitch('A').transpose(Pitch.OCTAVE), startBeat, numBeats);
+        startBeat += 1;
+        player.addNote(piano, new Pitch('F').transpose(Pitch.OCTAVE), startBeat, .5);
+        startBeat += .5;
+        player.addNote(piano, new Pitch('G').transpose(Pitch.OCTAVE), startBeat, .5);
+        startBeat += .5;
+        
+        //end measure
+        
+        startBeat += .5;   // .5 beat rest
+        
+        player.addNote(piano, new Pitch('E').transpose(Pitch.OCTAVE), startBeat, numBeats);
+        startBeat += 1;
+        player.addNote(piano, new Pitch('C').transpose(Pitch.OCTAVE), startBeat, .5);
+        startBeat += .5;
+        player.addNote(piano, new Pitch('D').transpose(Pitch.OCTAVE), startBeat, .5);
+        startBeat += .5;
+        player.addNote(piano, new Pitch('B'), startBeat, 3.0/4);
+        startBeat += 3.0/4;
+        
+        startBeat += 3.0/4;   // 3/4 beat rest
+        
+        
+        
+        // add a listener at the end of the piece to tell main thread when it's done
+        Object lock = new Object();
+        player.addEvent(startBeat, (Double beat) -> {
+            synchronized (lock) {
+                lock.notify();
+            }
+        });
+        
+        // print the configured player
+        System.out.println(player);
+
+        // play!
+        player.play();
+        
+        // wait until player is done
+        // (not strictly needed here, but useful for JUnit tests)
+        synchronized (lock) {
+            try {
+                lock.wait();
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+        System.out.println("done playing");
+        
+    }
     
     
 }
