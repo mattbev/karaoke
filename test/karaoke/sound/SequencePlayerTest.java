6dc321bd14413d9cd85087fe51b993072c6a5bf6
Sophia <sophias@mit.edu>
2018-05-03 22:52:07 -0400

    implemented testPiece3 without lyrics

--- test/karaoke/sound/SequencePlayerTest.java
+++ test/karaoke/sound/SequencePlayerTest.java
@@ -10,10 +10,10 @@ import org.junit.Test;
 public class SequencePlayerTest {
     public static final int OCTAVE = 12;
 
-    // TODO: warmup #2
+
     /**
      * Tetsing Strategy:
-     * 
+     * @category no_didit
      * Listening test on
      *  -piece1.abc
      *  -piece2.abc
@@ -281,6 +281,106 @@ public class SequencePlayerTest {
         }
         System.out.println("done playing");
         
+    }
+    
+    //Covers:
+    //      piece3.abc
+    @Test
+    public void testPiece3() throws MidiUnavailableException, InvalidMidiDataException{
+        Instrument piano = Instrument.PIANO;
+
+        // create a new player
+        final int beatsPerMinute = 100; // a beat is a quarter note, so this is 120 quarter notes per minute
+        final int ticksPerBeat = 64; // allows up to 1/64-beat notes to be played with fidelity
+        SequencePlayer player = new MidiSequencePlayer(beatsPerMinute, ticksPerBeat);
+        
+        // addNote(instr, pitch, startBeat, numBeats) schedules a note with pitch value 'pitch'
+        // played by 'instr' starting at 'startBeat' to be played for 'numBeats' beats.
+        
+        double startBeat = 0;
+        double numBeats = 1.0/2;
+        
+        startBeat += 4 * numBeats;   // 2 beat rest
+        
+        player.addNote(piano, new Pitch('D'), startBeat++, 1);
+        startBeat += 1;
+        // end measure
+        
+        player.addNote(piano, new Pitch('G'), startBeat++, 2);
+        startBeat += 2;
+        
+        player.addNote(piano, new Pitch('B'), startBeat++, 3.0/4);
+        startBeat += 3.0/4;
+        player.addNote(piano, new Pitch('G'), startBeat++, 3.0/4);
+        startBeat += 3.0/4;
+        //end measure and tuplet
+        
+        player.addNote(piano, new Pitch('B'), startBeat++, 2);
+        startBeat += 2;
+        player.addNote(piano, new Pitch('A'), startBeat++, 1);
+        startBeat += 1;
+        //end measure
+        
+        player.addNote(piano, new Pitch('G'), startBeat++, 2);
+        startBeat += 2;
+        player.addNote(piano, new Pitch('E'), startBeat++, 1);
+        startBeat += 1;
+        //end measure
+        
+        player.addNote(piano, new Pitch('D'), startBeat++, 2);
+        startBeat += 2;
+        player.addNote(piano, new Pitch('D'), startBeat++, 1);
+        startBeat += 1;
+        //end measure
+        
+        player.addNote(piano, new Pitch('G'), startBeat++, 2);
+        startBeat += 2;
+        
+        player.addNote(piano, new Pitch('B'), startBeat++, 3.0/4);
+        startBeat += 3.0/4;
+        player.addNote(piano, new Pitch('G'), startBeat++, 3.0/4);
+        startBeat += 3.0/4;
+        //end measure and tuplet
+        
+        player.addNote(piano, new Pitch('B'), startBeat++, 2);
+        startBeat += 2;
+        player.addNote(piano, new Pitch('A'), startBeat++, 1);
+        startBeat += 1;
+        //end measure
+        
+        player.addNote(piano, new Pitch('D').transpose(Pitch.OCTAVE), startBeat++, 3);
+        startBeat += 3;
+        //end measure
+        
+        
+        
+        
+        
+        
+        // add a listener at the end of the piece to tell main thread when it's done
+        Object lock = new Object();
+        player.addEvent(startBeat, (Double beat) -> {
+            synchronized (lock) {
+                lock.notify();
+            }
+        });
+        
+        // print the configured player
+        System.out.println(player);
+
+        // play!
+        player.play();
+        
+        // wait until player is done
+        // (not strictly needed here, but useful for JUnit tests)
+        synchronized (lock) {
+            try {
+                lock.wait();
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+        System.out.println("done playing");
     }
     
     
