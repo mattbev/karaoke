1ebdc83b0eba7d8d8121109f091f70992ec5e85a
Sophia <sophias@mit.edu>
2018-05-14 15:54:59 -0400

    changed karaoke class

--- src/karaoke/Playable.java
+++ src/karaoke/Playable.java
@@ -11,7 +11,7 @@ import karaoke.sound.SequencePlayer;
  * An immutable ADT to represent one syllable component in a piece of music that has a lyric syllable
  *
  */
-public interface Playable extends Karaoke {
+public interface Playable extends Music {
     
     // Datatype definition:
     //      Playable = Chord(notes: List<Note>, lyric: List<Lyric>)
