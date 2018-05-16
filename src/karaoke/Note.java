6db4a2ea45dd36ee947dbe448c7283114bc95e6f
Matthew Beveridge <mattbev@mit.edu>
2018-05-16 10:39:08 -0400

    note duration

--- src/karaoke/Note.java
+++ src/karaoke/Note.java
@@ -112,7 +112,7 @@ public class Note {
         if (denominator.length() > 0) {
             denominatorInt = Integer.parseInt(denominator);
         }
-        final double noteDuration = ((double) numeratorInt) / ((double) denominatorInt) * header.getDefaultLengthDouble();
+        final double noteDuration = ((double) numeratorInt) / ((double) denominatorInt) * header.getDefaultLengthDouble() / 4.;
         
         this.accidental = noteAccidental;
         this.duration = noteDuration;
